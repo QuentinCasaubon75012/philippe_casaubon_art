3841a05245d216db6cef865951205e5a740f0c00
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-03-09 11:24:22 +0100

    wait

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309093826) do
+ActiveRecord::Schema.define(version: 20170309095252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170309093826) do
     t.integer  "price"
     t.string   "name"
     t.string   "description"
+    t.string   "image"
   end
 
 end
